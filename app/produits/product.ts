0b03a7a0e58157f60d4e2e8196648af658e2f25e
NK-972 <nicolascastry@gmail.com>
2019-12-30 01:08:48 +0100

    1 file modified

--- app/produits/product.ts
+++ app/produits/product.ts
@@ -1,16 +1,16 @@
 export interface Product {
-    id : ''
-  , Prix : 0
-  , RD : 0
-  , Installations: ['']
-  , InstallationSelected: ''
-  , Production: 0
-  , Qualite: 0
-  , IP: 0
-  , IT: 0
-  , Notoriete: 0
-  , Demande: 0
-  , Stock: 0
-  , MB: 0
-  , TMB: 0
+    id : string
+  , Prix : Number
+  , RD : Number
+  , Installations: string[]
+  , InstallationSelected: string
+  , Production: Number
+  , Qualite: Number
+  , IP: Number
+  , IT: Number
+  , Notoriete: Number
+  , Demande: Number
+  , Stock: Number
+  , MB: Number
+  , TMB: Number
 }
